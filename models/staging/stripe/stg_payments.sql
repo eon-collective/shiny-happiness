be949d692882afc0557265ce2b5a73abf9a5a3cc
Daniel Malungu <daniel.malungu@skylitemage.com>
2023-10-26 13:32:36 +0000

    update: macro advanced

--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -7,10 +7,3 @@ select
     created as created_at
     
 from {{source('stripe', 'payment')}}
-{% set payment_methods = dbt_utils.get_column_values(
-        table=ref('stg_payments'),
-        column='payment_method',
-        order_by='max(created_at) desc',
-        max_records=50,
-        default=['bank_transfer', 'coupon', 'credit_card']
-%}
