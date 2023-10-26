f5ffbed837a33a1d0c253c0b7ce3f7d671799aa3
Daniel Malungu <daniel.malungu@skylitemage.com>
2023-10-26 09:53:59 +0000

    update: packages

--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -5,5 +5,12 @@ select
     status,
     {{cents_to_dollar('amount', 2)}} as amount,
     created as created_at
-
+    
 from {{source('stripe', 'payment')}}
+{% set payment_methods = dbt_utils.get_column_values(
+        table=ref('stg_payments'),
+        column='payment_method',
+        order_by='max(created_at) desc',
+        max_records=50,
+        default=['bank_transfer', 'coupon', 'credit_card']
+%}
