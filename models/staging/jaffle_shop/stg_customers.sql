55969a68854dd96df6f37e97b14a4208ffbde7ac
danielmalungu <danielkmalungu@gmail.com>
2023-10-30 14:56:55 +0300

    refactoring: Final status of the models and documentation.

--- models/staging/jaffle_shop/stg_customers.sql
+++ models/staging/jaffle_shop/stg_customers.sql
@@ -3,7 +3,8 @@ with customers as (
     select
         id as customer_id,
         first_name,
-        last_name
+        last_name,
+        first_name || ' ' || last_name as full_name
 
     from {{source('jaffle_shop', 'customers')}}
 
